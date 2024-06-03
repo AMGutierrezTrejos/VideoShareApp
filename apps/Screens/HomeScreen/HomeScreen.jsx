af606ff8bde8f83e45f3a44260e215968e1d1f36
MauroTre <m.gutierrez7@hotmail.com>
2024-06-02 22:50:28 -0500

    cambios

--- apps/Screens/HomeScreen/HomeScreen.jsx
+++ apps/Screens/HomeScreen/HomeScreen.jsx
@@ -80,4 +80,14 @@ export default function HomeScreen() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    paddingTop: 25,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  }, //remove
+});
